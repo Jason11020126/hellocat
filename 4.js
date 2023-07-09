8ffb6e9cb9ebe0791f46a5f1ec7c2e9a04f4d290
Jason11020126 <138146020+Jason11020126@users.noreply.github.com>
2023-07-09 18:53:40 +0800

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,5 +1,5 @@
 function goToNextPage(event) {
     var buttonId = event.target.id;
-    if(buttonId=="1")window.location.href = "https://jason11020126.github.io/hellocat/angry";
-    else window.location.href = "https://jason11020126.github.io/hellocat/forgive";
+    if(buttonId=="1")window.location.href = "https://jason11020126.github.io/hellocat/forgive";
+    else window.location.href = "https://jason11020126.github.io/hellocat/angry";
   }
